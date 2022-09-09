ced226e95e6e6c2209c50e40dbebbf3720cb9347
Coltensiemer <colten50@hotmail.com>
2022-09-08 20:03:49 -0400

    updated

---
+++ Testing/Learning/JavaScript /Java.js
@@ -0,0 +1,28 @@
+// let name = "Funny"; 
+// name = 'Hello';
+// console.log(name); 
+
+// let person = { 
+//     name: 'Colten',
+//     age: 27
+// };
+
+
+//dot notation 
+// person.age = 30; 
+
+// person['name'] = 'Mary';
+
+// console.log(person); 
+
+// let selectColor=['red', 'blue'];
+
+// let selectColor[2]= 'green';
+
+// console.log(selectColor);
+
+function greet(name,lastName){ 
+    console.log('Hello World ' + name + '' + lastName); 
+}
+
+greet('Colten','Siemer'); 
