0a317daae0182ca75f41ab04c5cbeb153f31c22f
Coltensiemer <colten50@hotmail.com>
2022-09-11 17:06:24 -0400

    update

--- Testing/Learning/JavaScript /java3.js
+++ Testing/Learning/JavaScript /java3.js
@@ -1,40 +1,39 @@
 //selectors 
 const toDoInput = document.querySelector('.todo-input'); 
 const toDoButton = document.querySelector('.todo-button'); 
-const toDoContainer= document.querySelector('.todo-container'); 
+const toDoList= document.querySelector('.todo-list'); 
 
 
 // Event Listeners 
-toDoButton.addEventListener('click', 'addToDo');
+toDoButton.addEventListener('click', addToDo);
 
 //Functions 
 
 function addToDo(event) { 
 
-
 //Prevent Form from submit 
-.event.preventDefault(); 
+// event.preventDefault(); 
 
-// Todo 
+// Todo div 
 const toDoDiv = document.createElement("div");
-toDoDiv.classList.add("todo"); 
+toDoDiv.classList.add(".todo"); 
 // create IL 
 const newToDo = document.createElement("li");
 newToDo.innerText = "hello"; 
-newToDo.classList.add("item");
-toDoDiv.appendChild("newToDo");
+newToDo.classList.add(".todo-item");
+toDoDiv.appendChild(newToDo);
 //Completed 
 const completeButton = document.createElement("button"); 
-completeButton.innerText("It works!"); 
-completeButton.classList.add("complete"); 
-newToDo.appendChild("completeButton"); 
+completeButton.innerText = "It works!"; 
+completeButton.classList.add(".todo-complete"); 
+toDoDiv.appendChild(completeButton); 
 
 // Delete Button 
 const deleteButton = document.createElement("button"); 
-deleteButton.innerText("It works!"); 
-deleteButton.classList.add("complete"); 
-newToDo.appendChild("deleteButton"); 
+deleteButton.innerText ="It works!"; 
+deleteButton.classList.add(".todo-delete"); 
+toDoDiv.appendChild(deleteButton); 
 /// append to todo-list
-todoList.appendChild(toDoDiv); 
+toDoList.appendChild(addToDiv);
 
 } 
